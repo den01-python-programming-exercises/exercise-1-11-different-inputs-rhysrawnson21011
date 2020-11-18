dbb1fad2a37a3362ab0bdbcdbeeaff9a56d98f51
rhysrawnson2101 <Rhysrawnson@gmail.com>
2020-11-18 11:41:25 +0000

    different inputs

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,15 @@
 def main():
     #write your code below this line
+    string = (input('give a string:')) 
+    number = (input('give a number:'))
+    float = (input('give a float:'))
+    boolean = (input('give a boolean:'))
 
+    print('you gave the string:' + string)
+    print('you gave the integer:' + number)
+    print('you gave the float:' + float)
+    print('you gave the boolean:' + boolean)
+    
 if __name__ == '__main__':
     main()
+ 
